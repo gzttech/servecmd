df5e360cd461e8d827267d72f7685b40da2191af
socrateslee <socrateslee@users.noreply.github.com>
2025-04-01 10:07:26 +0800

    fix package name

--- servecmd/__init__.py
+++ servecmd/__init__.py
@@ -2,6 +2,6 @@
 import importlib.metadata
 
 try:
-    __version__ = importlib.metadata.version("paipe")
+    __version__ = importlib.metadata.version("servecmd")
 except importlib.metadata.PackageNotFoundError:
     __version__ = "0.0.0"
